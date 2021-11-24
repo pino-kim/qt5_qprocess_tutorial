914c43fb3eee849f4a441b40bf5920e1d512add8
sungwon.kim <sungwon.pino@gmail.com>
2021-11-24 22:20:05 +0900

    Print QString to textBrowser if script run success

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,6 +21,13 @@ void MainWindow::on_pushButton_runScript_clicked()
     int ret;
     ret = this->demo->run_test_script();
     qDebug() << "ret :" << ret;
+
+    if( ret < 0) {
+        qDebug() << "[QT5 DBG] Return value" << ret << ", do nothing";
+    } else {
+        qDebug() << "[QT5 DBG] Print stdout of script to textBrowser";
+        ui->textBrowser_printStdout->setPlainText(demo->stdout_str);
+    }
 }
 
 
